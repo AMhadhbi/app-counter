64770b356a9e7969a2ae164fc54f93e46c46ea65
Achraf Mhadhbi <machraf@bloopark.de>
2021-02-28 18:02:09 +0100

    [Add] improve navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
+import React from "react";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
+    <React.Fragment>
       <Navbar/>
+      <main className='container'>
+        <button className='btn btn-info'>Reset</button>
+      </main>
+    </React.Fragment>
   );
 }
 
