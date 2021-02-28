48511d0a1189dbc1dba8f9a73db9d082b9a9a785
Achraf Mhadhbi <machraf@bloopark.de>
2021-02-28 18:35:33 +0100

    [Add] add map counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
-
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
       <main className='container'>
-        <button className='btn btn-info'>Reset</button>
         <Counters/>
       </main>
     </React.Fragment>
