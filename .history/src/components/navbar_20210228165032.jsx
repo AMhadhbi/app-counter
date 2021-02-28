5a8829ea457d2559f33f83288952fc78e7a7fa84
Achraf Mhadhbi <machraf@bloopark.de>
2021-02-28 17:46:10 +0100

    [delete] delete history

--- .history/src/components/navbar_20210228165032.jsx
+++
@@ -1,6 +0,0 @@
-
-
-
-<nav className="navbar navbar-light bg-light">
-  <span className="navbar-brand mb-0 h1">Navbar</span>
-</nav>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,11 @@ const  Navbar = () => {
     return ( 
     <nav className="navbar navbar-light bg-light h3">
         <a href='#'>
-            <img className ="rounded" src='images/me.png' alt=""
+            <img className ="rounded mr-2" src='images/me.png' alt=""
             style={{width: "40px", height: "40px"}}
             />
             Achraf Counter
-            <span className="badge badge-pill badge-success">0</span>
+            <span className="badge badge-pill badge-success ml-2">0</span>
         </a>
     </nav> 
     );
